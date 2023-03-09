dbb7018f79ffd661e9681b468037dbb9fc9ec055
wayne-AF <wayne.anthony.feeney@gmail.com>
2023-03-09 18:51:16 +0000

    created LikeSerializer

--- comments/views.py
+++ comments/views.py
@@ -21,4 +21,3 @@ class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
-    
---
+++ likes/serializers.py
@@ -0,0 +1,24 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Like
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Like model
+    The create method handles the unique constrain on 'owner' and 'post'
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = ['id', 'created_at', 'owner', 'post']
+
+    # preventing user from liking same post twice
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityErro:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
